c783931754a93473dad798a185544247706c918c
Srikanth Jeeva <sri.jjhero@gmail.com>
2012-09-11 14:31:51 +0530

    products CRUD

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+S21::Application.config.secret_token = '52d1521f7e9d1b3f1393815f0df761b4ab82c9fc1db751eb63e64f8fb85b0b15c64ad6179749f39cab9303693ca04f20267acccd792d9406343af5072db3fcb4'
